bcbd11069091368d61d7f8671e6a80161c93cc59
Bheemesh1706 <bheemeshbala.71@gmail.com>
2021-04-28 21:44:21 +0530

    Upgraded to postgress

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 2021_03_04_080726) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "article_categories", force: :cascade do |t|
     t.integer "article_id"
     t.integer "category_id"
@@ -20,8 +23,8 @@ ActiveRecord::Schema.define(version: 2021_03_04_080726) do
   create_table "articles", force: :cascade do |t|
     t.string "title"
     t.text "description"
-    t.datetime "created_at"
-    t.datetime "updated_at"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
     t.integer "user_id"
   end
 
