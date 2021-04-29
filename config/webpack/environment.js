e4e5e457250ff4645b4582df1c0e84071b386ae3
Bheemesh1706 <bheemeshbala.71@gmail.com>
2021-04-29 10:19:16 +0530

    webpacker error resolved

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,13 +1,3 @@
 const { environment } = require('@rails/webpacker')
-const WebpackDevServer = require('webpack-dev-server')
-
-const webpack = require("webpack")
-
-environment.plugins.append("Provide", new webpack.ProvidePlugin({
-    $: 'jquery',
-    jQuery: 'jquery',
-    Popper: ['popper.js','default']
-}))
-
 
 module.exports = environment
---
+++ src/index.js
@@ -0,0 +1,1 @@
+//Hope this removes the error
